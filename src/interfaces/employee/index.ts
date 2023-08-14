3e7e54777b47b235637575632bbebc5e646ab50f
roq-ai <>
2023-08-14 09:29:32 +0000

    Initial commit

---
+++ src/interfaces/employee/index.ts
@@ -0,0 +1,26 @@
+import { PayrollInterface } from 'interfaces/payroll';
+import { UserInterface } from 'interfaces/user';
+import { GetQueryInterface } from 'interfaces';
+
+export interface EmployeeInterface {
+  id?: string;
+  first_name: string;
+  last_name: string;
+  vacation_status: string;
+  user_id?: string;
+  created_at?: any;
+  updated_at?: any;
+  payroll?: PayrollInterface[];
+  user?: UserInterface;
+  _count?: {
+    payroll?: number;
+  };
+}
+
+export interface EmployeeGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  first_name?: string;
+  last_name?: string;
+  vacation_status?: string;
+  user_id?: string;
+}
